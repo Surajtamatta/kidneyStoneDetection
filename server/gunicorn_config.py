d54111d6e02864f786b96934c8ecc9bfeac60cc9
Surajtamatta <87592868+Surajtamatta@users.noreply.github.com>
2024-07-14 22:25:35 +0530

    Update gunicorn_config.py
    change gunicorn_config.py

--- server/gunicorn_config.py
+++ server/gunicorn_config.py
@@ -1,2 +1,16 @@
-workers = 4  # Adjust based on your server's resources
+# gunicorn_config.py
+
+# Number of worker processes, adjust based on your server's resources
+workers = 4
+
+# IP address and port to bind
 bind = '0.0.0.0:8080'
+
+# Timeout settings (adjust as needed)
+timeout = 120  # 120 seconds
+
+# Log level
+loglevel = 'info'
+
+# Access log filename (- for stdout)
+accesslog = '-'  
